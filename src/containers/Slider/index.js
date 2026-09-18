013d714098e374a5adc3e100ec61159c03aff744
Guillaume Vial <gvial.dev@gmail.com>
2026-09-18 14:52:11 +0200

    test : Revert failure unit test Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,8 +9,8 @@ const Slider = () => {
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus
     ? [...data.focus].sort(
-        //(evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)
-        (evtA, evtB) => new Date(evtA.date) - new Date(evtB.date)
+        (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)
+        //(evtA, evtB) => new Date(evtA.date) - new Date(evtB.date)
       )
     : [];
   const nextCard = () => {
