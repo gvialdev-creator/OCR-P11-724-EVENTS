06bbf0aee995b5de70dbe02dfbeb55dc0ea8a22f
Guillaume Vial <gvial.dev@gmail.com>
2026-09-18 15:33:54 +0200

    Test : revert Fail Unit test Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,7 +9,7 @@ const Slider = () => {
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus
     ? [...data.focus].sort(
-        (evtA, evtB) => new Date(evtA.date) - new Date(evtB.date)
+        (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)
       )
     : [];
   const nextCard = () => {
