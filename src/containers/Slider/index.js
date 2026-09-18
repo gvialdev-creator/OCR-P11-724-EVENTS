5c742bae82116590206e54b5314b60f6e6c96180
Guillaume Vial <gvial.dev@gmail.com>
2026-09-18 15:17:23 +0200

    Test : Fail unit test Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -9,7 +9,7 @@ const Slider = () => {
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus
     ? [...data.focus].sort(
-        (evtA, evtB) => new Date(evtB.date) - new Date(evtA.date)        
+        (evtA, evtB) => new Date(evtA.date) - new Date(evtB.date)
       )
     : [];
   const nextCard = () => {
